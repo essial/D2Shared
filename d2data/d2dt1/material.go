0cc96b3be72257c5b7d4e4cedaf58b543befb93f
Ziemas <ziemas@ziemas.se>
2019-12-20 01:46:08 +0100

    Fill out tile material flags
    Thanks to Necrolis and Nefarius

--- d2data/d2dt1/material.go
+++ d2data/d2dt1/material.go
@@ -2,11 +2,29 @@ package d2dt1
 
 // Lots of unknowns for now
 type MaterialFlags struct {
-	Animated bool // Is only lava animated? Might just be a lava flag
+	Other        bool
+	Water        bool
+	WoodObject   bool
+	InsideStone  bool
+	OutsideStone bool
+	Dirt         bool
+	Sand         bool
+	Wood         bool
+	Lava         bool
+	Snow         bool
 }
 
 func NewMaterialFlags(data uint16) MaterialFlags {
 	return MaterialFlags{
-		Animated: data&0x0100 == 0x0100,
+		Other:        data&0x0001 == 0x0001,
+		Water:        data&0x0002 == 0x0002,
+		WoodObject:   data&0x0004 == 0x0004,
+		InsideStone:  data&0x0008 == 0x0008,
+		OutsideStone: data&0x0010 == 0x0010,
+		Dirt:         data&0x0020 == 0x0020,
+		Sand:         data&0x0040 == 0x0040,
+		Wood:         data&0x0080 == 0x0080,
+		Lava:         data&0x0100 == 0x0100,
+		Snow:         data&0x0400 == 0x0400,
 	}
 }
